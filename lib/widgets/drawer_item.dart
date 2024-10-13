9ae0e903bffb2c92f8a46538ce6991eed24b3c9f
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-13 12:18:53 -0700

    Create active drawer item

--- lib/widgets/drawer_item.dart
+++ lib/widgets/drawer_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
-import 'package:responsive_dash_board/utils/app_style.dart';
+import 'package:responsive_dash_board/widgets/inactivedraweritem_and_activedraweritem.dart';
 
 class DrawerItem extends StatelessWidget {
   const DrawerItem(
@@ -10,12 +9,8 @@ class DrawerItem extends StatelessWidget {
   final bool isActive;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleRegular16,
-      ),
-    );
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
   }
 }
