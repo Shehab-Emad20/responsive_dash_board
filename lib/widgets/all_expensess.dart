6181afe544fd000965456ace16e1a8a96ea01ba0
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-14 18:55:19 -0700

    All expenses header part 1

--- lib/widgets/all_expensess.dart
+++ lib/widgets/all_expensess.dart
@@ -5,6 +5,15 @@ class AllExpenses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: const EdgeInsets.all(20),
+        decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            )),
+        child: Column(
+          children: [],
+        ));
   }
 }
