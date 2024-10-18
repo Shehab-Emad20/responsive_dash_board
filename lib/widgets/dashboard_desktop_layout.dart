d246a8ced1eaf8bf1336b5dd77e6c999bc882bdd
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-17 21:35:04 -0700

    Quick invoice part one

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -14,7 +15,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         Expanded(
             flex: 2,
             child: Column(
-              children: [Expanded(child: AllExpenses())],
+              children: [AllExpenses(), QuickInvoice()],
             ))
       ],
     );
