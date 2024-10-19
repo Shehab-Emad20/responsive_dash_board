dbcaeb049d755d418e2b7c767ba342f880a6bd20
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-19 13:20:09 -0700

    My card done

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -20,6 +20,7 @@ class MyCard extends StatelessWidget {
           ),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
               contentPadding:
@@ -29,7 +30,27 @@ class MyCard extends StatelessWidget {
                       AppStyles.styleRegular16.copyWith(color: Colors.white)),
               subtitle: Text('Syah Bandi', style: AppStyles.styleMedium20),
               trailing: SvgPicture.asset(Assets.imagesGallery),
-            )
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 48 - 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '0918 8124 8129',
+                    style:
+                        AppStyles.styleSemiBold24.copyWith(color: Colors.white),
+                  ),
+                  Text(
+                    '12/20 - 124',
+                    style:
+                        AppStyles.styleRegular16.copyWith(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 54 - 28),
           ],
         ),
       ),
