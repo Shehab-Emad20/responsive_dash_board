d90d6b0496f280fcb4a0f619ed03ee78193d9c3c
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-20 14:47:06 -0700

    Dots indicator

---
+++ lib/widgets/dots_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_dot.dart';
+
+class DotsIndicator extends StatelessWidget {
+  const DotsIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(
+          3,
+          (index) => const Padding(
+                padding: EdgeInsets.only(right: 6.0),
+                child: CustomDot(isActive: false),
+              )),
+    );
+  }
+}
