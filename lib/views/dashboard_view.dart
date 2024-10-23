2ae1066d372abc82dc3b5b13b826551b3795417a
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-22 21:58:58 -0700

    tablet layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
 import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_tablet_Layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
@@ -11,7 +12,7 @@ class DashboardView extends StatelessWidget {
         backgroundColor: const Color(0xFFF7F9FA),
         body: AdaptiveLayout(
           mobileLayout: (context) => const SizedBox(),
-          desktopLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DashboardTabletLayout(),
           tabletLayout: (context) => const DashboardDesktopLayout(),
         ));
   }
