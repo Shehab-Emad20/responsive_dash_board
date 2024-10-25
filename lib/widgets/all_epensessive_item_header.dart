8078fad9727cb29d86e7cc09cc78e4818abfe766
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-25 14:36:34 -0700

    Responsive mobile layout part 2

--- lib/widgets/all_epensessive_item_header.dart
+++ lib/widgets/all_epensessive_item_header.dart
@@ -13,18 +13,21 @@ class AllEpensessiveItemHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          height: 60,
-          width: 60,
-          decoration: ShapeDecoration(
-            color: imageBackgroundColor ?? const Color(0xFFFAFAFA),
-            shape: const OvalBorder(),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(14),
-            child: SvgPicture.asset(image,
-                colorFilter: ColorFilter.mode(
-                    imageColor ?? const Color(0xff4EB7F2), BlendMode.srcIn)),
+        Flexible(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              decoration: ShapeDecoration(
+                color: imageBackgroundColor ?? const Color(0xFFFAFAFA),
+                shape: const OvalBorder(),
+              ),
+              child: Center(
+                child: SvgPicture.asset(image,
+                    colorFilter: ColorFilter.mode(
+                        imageColor ?? const Color(0xff4EB7F2),
+                        BlendMode.srcIn)),
+              ),
+            ),
           ),
         ),
         const Spacer(),
