4d7c513449fffc5a01f5f773c7b468d3d4c744f8
Shehab-Emad20 <shehabz2014@gmail.com>
2024-10-24 19:22:32 -0700

    Detailed income chart part one

---
+++ lib/widgets/dtailed_icome_chart.dart
@@ -0,0 +1,52 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class DtailedIcomeChart extends StatefulWidget {
+  const DtailedIcomeChart({super.key});
+
+  @override
+  State<DtailedIcomeChart> createState() => _DtailedIcomeChart();
+}
+
+class _DtailedIcomeChart extends State<DtailedIcomeChart> {
+  int activIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(aspectRatio: 1, child: PieChart(getChartData()));
+  }
+
+  PieChartData getChartData() {
+    return PieChartData(
+        pieTouchData: PieTouchData(
+          enabled: true,
+          touchCallback: (p0, pieTouchRespons) {
+            activIndex =
+                pieTouchRespons?.touchedSection?.touchedSectionIndex ?? -1;
+            setState(() {});
+          },
+        ),
+        sectionsSpace: 0,
+        sections: [
+          PieChartSectionData(
+              showTitle: false,
+              value: 40,
+              radius: activIndex == 0 ? 60 : 50,
+              color: const Color(0xFF208BC7)),
+          PieChartSectionData(
+              radius: activIndex == 1 ? 60 : 50,
+              showTitle: false,
+              value: 25,
+              color: const Color(0xFF4DB7F2)),
+          PieChartSectionData(
+              radius: activIndex == 2 ? 60 : 50,
+              showTitle: false,
+              value: 20,
+              color: const Color(0xFF064060)),
+          PieChartSectionData(
+              radius: activIndex == 3 ? 60 : 50,
+              showTitle: false,
+              value: 22,
+              color: const Color(0xFFE2DECD)),
+        ]);
+  }
+}
--- lib/widgets/incom_section_body.dart
+++ lib/widgets/incom_section_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/size_config.dart';
+import 'package:responsive_dash_board/widgets/dtailed_icome_chart.dart';
 import 'package:responsive_dash_board/widgets/income_chart.dart';
 import 'package:responsive_dash_board/widgets/income_detalis.dart';
 
@@ -12,7 +13,7 @@ class IncomSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
     return width >= SizeConfig.desktop && width < 1750
-        ? const SizedBox()
+        ? const Expanded(child: DtailedIcomeChart())
         : const Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
